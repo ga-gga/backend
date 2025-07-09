79316a2fc1c3c0a26670993699329d1d785a27f3
Wonwoo-Seo <so222234@gmail.com>
2025-07-09 15:38:04 +0900

    feat: 외부 api 메타스키마, api 파라미터 스키마 생성
    api 파라미터 스키마
    - 내부 주소 체계와 매핑을 위한 KoreanAddress 참조
    - 호출 api 추적을 위해 ApiMetadata 참조

---
+++ src/models/ApiParameter.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const apiParameterSchema = new mongoose.Schema(
+  {
+    externalCode: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    koreanAddressCode: {
+      type: String,
+      ref: 'KoreanAddress',
+      match: /^\d{10}$/,
+    },
+    apiMetadataId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ApiMetadata',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+apiParameterSchema.index({ apiMetadataId: 1, externalCode: 1 }, { unique: true });
+
+module.exports = mongoose.model('ApiParameter', apiParameterSchema);
